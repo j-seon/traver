7ff52d6fb7ba48e7265625074accf0b5184d2b44
SJ <jseon382@gmail.com>
2022-10-08 22:38:14 +0900

    gotraver-map_main 페이지의 css, js, jq 전부 따로 빼냈어

---
+++ traverSite/WebContent/file/jq/map_main_add_place.js
@@ -0,0 +1,33 @@
+// 일정추가 관련 스크립트
+
+function placeCategoryChange(placeCategory) {
+// 카테고리 변경
+	$.ajax({
+		type : "POST",
+		url : "/traverSite/placeList",
+		data : {
+			"placeCategory" : placeCategory, 
+		},
+		success : function(placeList) {
+			if (placeList.size() == 0) {
+				alert("카테고리 변경에 실패했습니다.\n다시 시도하세요.");
+			}
+		}
+	});
+}
+
+function placeSearchChange(searchKeyword) {
+// 검색어로 장소 리스트 변경
+	$.ajax({
+		type : "POST",
+		url : "/traverSite/placeList",
+		data : {
+			"searchKeyword" : searchKeyword, 
+		},
+		success : function(placeList) {
+			if (placeList.size() == 0) {
+				alert("검색에 실패했습니다.\n다시 시도하세요.");
+			}
+		}
+	});
+}
